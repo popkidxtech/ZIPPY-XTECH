e6fbb00c5e1be29251983851b51dad4bff03c7a5
popkidxtech <popkidbotdeployment@gmail.com>
2025-03-31 13:43:03 +0300

    ᴘᴏᴘᴋɪᴅ

--- plugins/popkidv.js
+++ plugins/popkidv.js
@@ -81,7 +81,7 @@ const yt = await ytsearch(q);
     if (yt.results.length < 1) return reply("No results found!");
     
     let yts = yt.results[0];  
-    let apiUrl = `https://apis.giftedtech.web.id/api/download/ytmp3?apikey=gifted&url=${encodeURIComponent(videoUrl)}`;
+    let apiUrl = `https://apis.davidcyriltech.my.id/youtube/mp3?url=${encodeURIComponent(yts.url)}`;
     
     let response = await fetch(apiUrl);
     let data = await response.json();
