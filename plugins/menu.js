3c3d6f2236821c6f7174a0ab9cc7ae3b3667d179
popkidxtech <popkidbotdeployment@gmail.com>
2025-04-01 14:47:39 +0300

    Update and rename menu2.js to menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -5,7 +5,7 @@ const {runtime} = require('../lib/functions')
 const axios = require('axios')
 
 cmd({
-    pattern: "menu2",
+    pattern: "menu",
     alias: ["allmenu","fullmenu"],use: '.menu',
     desc: "menu the bot",
     category: "menu",
