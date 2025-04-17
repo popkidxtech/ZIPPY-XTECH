a29ac83f624efdc7b6a084a5719575e7cf199041
popkidxtech <popkidbotdeployment@gmail.com>
2025-04-17 10:05:32 +0300

    Update menu2.js

--- plugins/menu2.js
+++ plugins/menu2.js
@@ -344,7 +344,7 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
         await conn.sendMessage(
             from,
             {
-                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/nex0fm.jpg' },
+                image: { url: config.MENU_IMAGE_URL || 'https://files.catbox.moe/lkmvah.jpg' },
                 caption: dec,
                 contextInfo: {
                     mentionedJid: [m.sender],
